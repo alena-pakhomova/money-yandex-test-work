b3d9d5e1db5dac80eed9814415379ca02e60e809
nik-pakhomov <nikpackhomov@gmail.com>
2018-09-30 19:28:06 +0300

    add build the whole project via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,71 @@
 
 const gulp = require("gulp");
 const concat = require("gulp-concat");
+const debug = require("gulp-debug");
 
-gulp.task("build", function() {
-    return gulp.src("./js/**/*.js")
-        .pipe(concat("app.js"))
+const customHtmlPath = [ 
+    "./index.html", 
+    "./page-card.html"
+];
+
+const vendorCssPath = [ 
+    "./node_modules/bootstrap/dist/css/bootstrap.min.css", 
+    "./node_modules/bootstrap/dist/css/bootstrap.min.css.map"
+];
+
+const vendorJsPath = [ 
+    "./node_modules/jquery/dist/jquery.min.js",
+    "./node_modules/jquery/dist/jquery.min.map",
+    "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+    "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map"
+];
+
+gulp.task("build:customHtml", function() {
+    return gulp.src(customHtmlPath)
         .pipe(gulp.dest("./dist"));
 });
 
-gulp.watch("./js/**/*.js", gulp.series("build"))
+gulp.task("build:customFont", function() {
+    return gulp.src("./font/*.ttf")
+        .pipe(gulp.dest("./dist/font"));
+});
+
+gulp.task("build:customImages", function() {
+    return gulp.src("./img/**")
+        .pipe(gulp.dest("./dist/img"));
+});
+
+gulp.task("build:customCss", function() {
+    return gulp.src("./css/style.css")
+        .pipe(debug({ title: "src" }))
+        .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("build:customJs", function() {
+    return gulp.src("./js/**/*.js")
+        .pipe(debug({ title: "src" }))
+        .pipe(concat("app.js"))
+        .pipe(debug({ title: "concat" }))
+        .pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task("build:vendorCss", function() {
+    return gulp.src(vendorCssPath)
+        .pipe(debug({ title: "src" }))
+        .pipe(gulp.dest("./dist/vendor/css"));
+});
+
+gulp.task("build:vendorJs", function() {
+    return gulp.src(vendorJsPath)
+        .pipe(debug({ title: "src" }))
+        .pipe(gulp.dest("./dist/vendor/js"));
+});
+
+gulp.task("build", gulp.parallel("build:customHtml",
+                                 "build:customFont", 
+                                 "build:customImages", 
+                                 "build:customCss", 
+                                 "build:customJs", 
+                                 "build:vendorCss", 
+                                 "build:vendorJs")
+);
